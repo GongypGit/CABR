6be7e8fa476180263361a30986479c3091c718df
buswinka <buswinka@g.harvard.edu>
2019-12-06 13:59:05 -0500

    added csv functionallity

--- CABR3.py
+++ CABR3.py
@@ -1,6 +1,7 @@
 from parser import EPL
 import glob
 import os
+import numpy as np
 import itertools
 
 p = EPL('/Users/cx926/Desktop/CABR/data/1191/ABR-1191-4-analyzed.txt')
@@ -15,6 +16,8 @@ class experiment:
 
         self.conditions = []
         self.animals = []
+        self.frequencies = []
+        freq_set = False
 
 
         for dirs in glob.glob(suppath + '/*/'):
@@ -27,55 +30,91 @@ class experiment:
             pathlist = glob.glob(self.suppath + condition+'/*/')
             animallist = []
 
-
+            experiment_classes = []
             for animal in pathlist:
 
                 animallist.append(os.path.basename(animal[0:-1:1]))
                 experiment_path_list = glob.glob(self.suppath + condition + '/' + os.path.basename(animal[0:-1:1]) + '/' + file_regex)
-                experiment_classes = []
+                animal_classes = []
+                if self.frequencies:
+                    freq_set = True
 
                 for experiment in experiment_path_list:
+                    if not freq_set:
+                        self.frequencies.append(self.parser(experiment).frequencies)
 
-                    experiment_classes.append(self.parser(experiment))
+
+                    animal_classes.append(self.parser(experiment))
+                experiment_classes.append(animal_classes)
+                print(experiment_classes)
             condition_dict.append(dict(zip(animallist, experiment_classes)))
 
+        print(condition_dict)
         self.experiment_dict = dict(zip(self.conditions, condition_dict))
+        self.frequencies.sort()
 
     @staticmethod
     def list_to_str(original):
         new = []
         while original:
             new.append(str(original.pop(0)))
-
         return new
-
 
     def write_csv(self):
 
-        freq = []
-        for condition in self.experiment_dict:
-            file = open(condition+'.csv','w')
+        for f in self.frequencies:
+            csvmat = []
+            strmat = []
+            csvmat.append(['frequency'] + [str(f)])
 
-            for animal in self.experiment_dict(condition):
+            file = open(str(f) + 'kHz_data.csv', 'w')
+            file = open(str(f) + 'kHz_data.csv', 'a')
 
-                if not freq:
-                    freq = self.experiment_dict[condition][animal].frequencies
+            for condition in self.experiment_dict:
+                print(condition)
+                csvmat.append(['Condition'] + [condition])
+                for animal in self.experiment_dict[condition]:
+                    print(animal)
+
+                    for run in self.experiment_dict[condition][animal]:
+
+                        if f == run.frequencies:
+
+                            csvmat.append(['Animal'] + [str(run.id)])
+                            csvmat.append(['Level']+self.list_to_str(run.level))
+                            csvmat.append(['Amplitudes'] + self.list_to_str(run.amplitudes))
+
+                # What im doing here is finding the maximum length of sublists
+                # contained within csvmat
+                # need to do this so I can create a perfectly rectangular list of lists
+                # which would be impossible with two long lists of condition, frequency, and animal
+                maxlen = 0
+                for i in range(len(csvmat)):
+                   if maxlen <= len(csvmat[i]):
+                       maxlen = len(csvmat[i])
+
+            for i in range(maxlen):
+                line = []
+                for j in range(len(csvmat)):
+                    try:
+                        line.append(str(csvmat[j][i]) + ',')
+                    except:
+                        line.append(' ,')
+                strmat.append(line)
+
+            for i in range(len(strmat)):
+                if i>0:
+                    file.write('\n')
+                for j in range(len(strmat[0])):
+                    file.write(strmat[i][j])
+
+            file.close()
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+        # for i in range(len(bigcsvmat))
 
 
 
@@ -109,5 +148,3 @@ class ABR():
 
 
 
-test = experiment('/Users/cx926/Desktop/CABR/Super/')
-test.write_csv()
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import parser
-#import CABR3
+import CABR3
 
 p = parser.EPL('/Users/cx926/Desktop/CABR/data/1191/ABR-1191-4-analyzed.txt')
 
 
 
+test = CABR3.experiment('/Users/cx926/Desktop/CABR/Super/')
+test.write_csv()
